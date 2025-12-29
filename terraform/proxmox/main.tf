ce6c3809644baa45b3899aa7db42cbc13f5fa788
Tagomori0211 <tagomoriyuukichi@gmail.com>
2025-12-29 23:33:11 +0900

    compose,main.tf fix

--- terraform/proxmox/main.tf
+++ terraform/proxmox/main.tf
@@ -1,108 +1,27 @@
-# ---------------------------------------------------------
-# 1. DevOps VM (Linked Clone)
-# ---------------------------------------------------------
-resource "proxmox_vm_qemu" "devops_vm" {
-  name        = "Monitor-vm"
-  target_node = "pve"
-  clone       = "ubuntu-2204-cloudinit-template"
+# proxmox_vm.tf
+resource "proxmox_vm_qemu" "minecraft_dashboard" {
+  name        = "minecraft-dashboard"
+  target_node = "mc-server"  # PROXMOXノード名
   
-  full_clone = false
-
+  # 5700G想定スペック
   cores   = 4
   sockets = 1
-  memory  = 8192
-  scsihw  = "virtio-scsi-pci"
-  bootdisk = "scsi0"
-
-  # 【修正】vga設定 (これはOK)
-  vga {
-    type = "std"
-  }
-
-  # 【削除】cloudinit_cdrom_storage = "local-lvm" は消す！
-
-  # 【追加】Cloud-Initドライブを disk ブロックとして定義！
+  memory  = 4096
+  
+  # ディスク
   disk {
-    slot    = "ide2"
-    type    = "cloudinit"
+    size    = "32G"
+    type    = "scsi"
     storage = "local-lvm"
   }
-
-  # メインディスク
-  disk {
-    slot     = "scsi0"
-    size     = "32G"
-    type     = "disk"
-    storage  = "local-lvm"
-    iothread = true
-  }
-
+  
+  # ネットワーク (10GbE想定)
   network {
-    id     = 0
     model  = "virtio"
     bridge = "vmbr0"
   }
-
-  os_type = "cloud-init"
+  
+  # Cloud-init設定
+  os_type   = "cloud-init"
   ipconfig0 = "ip=192.168.0.100/24,gw=192.168.0.1"
-  ciuser  = "tagomori"
-  cipassword = "password123"
-  sshkeys = <<EOF
-  ${var.ssh_public_key}
-  EOF
 }
-
-# ---------------------------------------------------------
-# 2. Minecraft VM (Full Clone)
-# ---------------------------------------------------------
-resource "proxmox_vm_qemu" "mcbe_server_vm" {
-  name        = "App-vm"
-  target_node = "pve"
-  clone       = "ubuntu-2204-cloudinit-template"
-
-  full_clone = true
-
-  cores   = 4
-  sockets = 1
-  memory  = 16384
-  scsihw  = "virtio-scsi-pci"
-  bootdisk = "scsi0"
-
-  # 【修正】vga設定
-  vga {
-    type = "std"
-  }
-
-  # 【追加】Cloud-Initドライブ
-  disk {
-    slot    = "ide2"
-    type    = "cloudinit"
-    storage = "local-lvm"
-  }
-
-  # メインディスク
-  disk {
-    slot     = "scsi0"
-    size     = "50G"
-    type     = "disk"
-    storage  = "local-lvm"
-    iothread = true
-  }
-
-  network {
-    id     = 0
-    model  = "virtio"
-    bridge = "vmbr0"
-  }
-
-  os_type = "cloud-init"
-  ipconfig0 = "ip=192.168.0.101/24,gw=192.168.0.1"
-  ciuser  = "tagomori"
-  cipassword = "password123"
-  sshkeys = <<EOF
-  ${var.ssh_public_key}
-  EOF
-}
-
-
-
